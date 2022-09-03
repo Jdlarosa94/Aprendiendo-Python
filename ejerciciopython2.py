2146b296b33bc1926758d4e7a53a2de60798a8cb
Jesus De la Rosa <jddlrm@gmail.com>
2022-09-02 21:46:19 -0400

    PracticaPyWombat

---
+++ ejerciciopython2.py
@@ -0,0 +1,16 @@
+libros = [
+    'Don Quijote de la Mancha',
+    'Historia de dos ciudades',
+    'El Señor de los Anillos',
+    'El principito',
+    'El hobbit',
+    'Sueño en el pabellón rojo',
+    'Las aventuras de Alicia en el país de las maravillas',
+    'Triple representatividad,',
+    'Y no quedó ninguno (Diez negritos)',
+    ' El león, la bruja y el armario'
+]
+if __name__ == '__main__':
+    print('Primer libro:', libros[0])
+    print('Libro Central:', libros[len(libros)//2])
+    print('Ultimo Libro:', libros[-1])
